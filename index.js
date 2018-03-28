340b51f9a30209ac40d67ab0d3e5e895b3aa93a4
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2018-03-28 13:58:36 -0700

    Displaying active users

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ var express  = require('express');
 var app = express();
 var server = require('http').Server(app).listen(3000);
 var io = require('socket.io')(server);
+var users = [];
 
+app.use(express.static(__dirname));
+app.use(express.static(require('path').join(__dirname, 'public')));
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket) {
-  console.log('socket connected');
+   console.log('socket connected');
+   socket.emit('displayUsers', {total : users.length, all_users : users});
+
+   socket.on('newUser', function(data) {
+     console.log("Hello, " + data);
+     users.push(data);
+     io.sockets.emit('appendUser', data);
+   });
+
   socket.on('disconnect', function() {
+    //users.remove()
     console.log('disconnected');
   });
 });
---
+++ public/client.js
@@ -0,0 +1,29 @@
+var socket = io.connect();
+var name = "";
+
+$(function() {
+  $('#formJoin').submit(function(e) {
+    e.preventDefault();
+    if($('#input-name').val() != "")
+    {
+      name = $('#input-name').val();
+      socket.emit('newUser', name);
+    }
+  });
+
+  function addUser(name)
+  {
+    $('#active-users').append('<li class="list-group-item">'+name+'</li>');
+  }
+
+  socket.on('displayUsers', function(data) {
+    var size = data.total;
+    for (var key in data.all_users)
+      addUser(data.all_users[key]);
+  });
+
+  socket.on('appendUser', function(data) {
+    addUser(data);
+  });
+
+});
