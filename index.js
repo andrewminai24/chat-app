cc806bebdd83ad3700ad28eef4ecbb6eea1b8e2c
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2018-03-28 11:47:49 -0700

    defining default route

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-var express = require('express');
+var express  = require('express');
 var app = express();
-var server = require('http').Server(app).listen(3000, function(){
-  console.log("listening on port 3000");
-});
+var server = require('http').Server(app).listen(3000);
 var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
-io.on('connection', function(socket){
-  console.log("Socket connected");
-  socket.on('disconnect', function() {
-    console.log("disconnected");
-  });
+  res.sendFile(__dirname + '/index.html');
 });
