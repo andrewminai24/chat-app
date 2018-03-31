bdaf18c8ea80e38b4611e16f51270ab23ca3b3ff
Enkhmanlai Michael Ganzorig <manlai1025@gmail.com>
2018-03-30 20:59:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express  = require('express');
 var app = express();
-var server = require('http').Server(app).listen(3000);
+var server = require('http').Server(app).listen(process.env.PORT || 3000);
 var io = require('socket.io')(server);
 var users = ["No Active Users"];
 var messages = [];
